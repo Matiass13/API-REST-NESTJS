f01a6a1b1ba293f3141e9d1b747038e637a34153
Matias <agueromatias664@gmail.com>
2024-04-19 17:06:07 -0300

    add news controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FabricantesController } from './controllers/fabricantes.controller';
+import { ProductosController } from './controllers/productos.controller';
+import { CompradoresController } from './controllers/compradores.controller';
+import { CategoriasController } from './controllers/categorias.controller';
+import { PedidosController } from './controllers/pedidos.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    FabricantesController,
+    ProductosController,
+    CompradoresController,
+    CategoriasController,
+    PedidosController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
