36ef9fec76a02f732d1cc8ee5e8f2ee7b35c4acd
Matias <agueromatias664@gmail.com>
2024-04-20 05:31:42 -0300

    added new functions

--- src/controllers/compradores.controller.ts
+++ src/controllers/compradores.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('compradores')
-export class CompradoresController {}
+export class CompradoresController {
+  @Get()
+  getCompradores(): string {
+    return 'Lista de compradores';
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return 'User with ID: ' + id;
+  }
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'acción de crear',
+      payload,
+    };
+  }
+}
